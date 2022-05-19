159454aab2158465a949f4e6dcb0e412d4c8c01a
Paul Morrow <pmorow93@gmail.com>
2022-05-19 00:36:10 -0500

    Class component functioning

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -3,9 +3,7 @@ import axios from "axios";
 
 export default class AppClass extends React.Component {
   state = {
-    onSquare: 4,
-    x: 2,
-    y: 2,
+    grid: [null, null, null, null, "B", null, null, null, null],
     steps: 0,
     email: "",
     message: "",
@@ -15,286 +13,233 @@ export default class AppClass extends React.Component {
     console.log(this.state);
   };
 
-  handleMoveUp = (e) => {
-    if (this.state.onSquare === 4) {
-      this.setState({
-        ...this.state,
-        onSquare: 1,
-        x: 2,
-        y: 1,
-        steps: 1,
+  handleMoveUp = () => {
+    const [_, y] = this.getCoordinates(this.state.grid);
+    if (y === 1) {
+      this.setState((state) => {
+        return {
+          ...state,
+          message: "You can't go up",
+        };
       });
-    } else if (this.state.onSquare === 3) {
-      this.setState({
-        ...this.state,
-        onSquare: 0,
-        x: 1,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 5) {
-      this.setState({
-        ...this.state,
-        onSquare: 2,
-        x: 3,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 6) {
-      this.setState({
-        ...this.state,
-        onSquare: 3,
-        x: 1,
-        y: 3,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 7) {
-      this.setState({
-        ...this.state,
-        onSquare: 4,
-        x: 2,
-        y: 2,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 8) {
-      this.setState({
-        ...this.state,
-        onSquare: 5,
-        x: 3,
-        y: 2,
-        steps: this.state.steps + 1,
+    }
+
+    if (y === 2 || y === 3) {
+      this.setState((state) => {
+        const originalIndex = state.grid.findIndex((activeSquare) =>
+          Boolean(activeSquare) ? activeSquare.toLowerCase() === "b" : null
+        );
+        const newGrid = state.grid.map(() => null);
+        newGrid[originalIndex - 3] = "B";
+
+        return {
+          ...state,
+          grid: newGrid,
+          message: "",
+          steps: state.steps + 1,
+        };
       });
     }
   };
 
-  handleMoveDown = (e) => {
-    if (this.state.onSquare === 4) {
-      this.setState({
-        ...this.state,
-        onSquare: 7,
-        x: 2,
-        y: 3,
-        steps: this.state.steps + 1,
+  // [null, null, null]
+  // [null, "B", null]
+  // [null, null, null]
+
+  handleMoveDown = () => {
+    const [_, y] = this.getCoordinates(this.state.grid);
+
+    if (y === 3) {
+      this.setState((state) => {
+        return {
+          ...state,
+          message: "You can't go down",
+        };
       });
-    } else if (this.state.onSquare === 3) {
-      this.setState({
-        ...this.state,
-        onSquare: 6,
-        x: 1,
-        y: 3,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 5) {
-      this.setState({
-        ...this.state,
-        onSquare: 8,
-        x: 3,
-        y: 3,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 0) {
-      this.setState({
-        ...this.state,
-        onSquare: 3,
-        x: 1,
-        y: 2,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 1) {
-      this.setState({
-        ...this.state,
-        onSquare: 4,
-        x: 2,
-        y: 2,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 2) {
-      this.setState({
-        ...this.state,
-        onSquare: 5,
-        x: 3,
-        y: 2,
-        steps: this.state.steps + 1,
+    }
+
+    if (y === 1 || y === 2) {
+      this.setState((state) => {
+        const originalIndex = state.grid.findIndex((activeSquare) =>
+          Boolean(activeSquare) ? activeSquare.toLowerCase() === "b" : null
+        );
+        const newGrid = state.grid.map(() => null);
+        newGrid[originalIndex + 3] = "B";
+
+        return {
+          ...state,
+          grid: newGrid,
+          message: "",
+          steps: state.steps + 1,
+        };
       });
     }
   };
 
-  handleMoveLeft = (e) => {
-    if (this.state.onSquare === 4) {
-      this.setState({
-        ...this.state,
-        onSquare: 3,
-        x: 2,
-        y: 2,
-        steps: this.state.steps + 1,
+  handleMoveLeft = () => {
+    const [x] = this.getCoordinates(this.state.grid);
+
+    if (x === 1) {
+      this.setState((state) => {
+        return {
+          ...state,
+          message: "You can't go left",
+        };
       });
-    } else if (this.state.onSquare === 5) {
-      this.setState({
-        ...this.state,
-        onSquare: 4,
-        x: 2,
-        y: 2,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 1) {
-      this.setState({
-        ...this.state,
-        onSquare: 0,
-        x: 1,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 2) {
-      this.setState({
-        ...this.state,
-        onSquare: 1,
-        x: 2,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 7) {
-      this.setState({
-        ...this.state,
-        onSquare: 6,
-        x: 1,
-        y: 3,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 8) {
-      this.setState({
-        ...this.state,
-        onSquare: 7,
-        x: 2,
-        y: 3,
-        steps: this.state.steps + 1,
+    }
+
+    if (x === 2 || x === 3) {
+      this.setState((state) => {
+        const originalIndex = state.grid.findIndex((activeSquare) =>
+          Boolean(activeSquare) ? activeSquare.toLowerCase() === "b" : null
+        );
+        const newGrid = state.grid.map(() => null);
+        newGrid[originalIndex - 1] = "B";
+
+        return {
+          ...state,
+          grid: newGrid,
+          message: "",
+          steps: state.steps + 1,
+        };
       });
     }
   };
 
-  handleMoveRight = (e) => {
-    if (this.state.onSquare === 4) {
-      this.setState({
-        onSquare: 5,
-        x: 3,
-        y: 2,
-        steps: this.state.steps + 1,
+  handleMoveRight = () => {
+    const [x] = this.getCoordinates(this.state.grid);
+
+    if (x === 3) {
+      this.setState((state) => {
+        return {
+          ...state,
+          message: "You can't go right",
+        };
       });
-    } else if (this.state.onSquare === 3) {
-      this.setState({
-        ...this.state,
-        onSquare: 4,
-        x: 2,
-        y: 2,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 1) {
-      this.setState({
-        ...this.state,
-        onSquare: 2,
-        x: 3,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 0) {
-      this.setState({
-        ...this.state,
-        onSquare: 1,
-        x: 2,
-        y: 1,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 6) {
-      this.setState({
-        ...this.state,
-        onSquare: 7,
-        x: 2,
-        y: 3,
-        steps: this.state.steps + 1,
-      });
-    } else if (this.state.onSquare === 7) {
-      this.setState({
-        ...this.state,
-        onSquare: 8,
-        x: 3,
-        y: 3,
-        steps: this.state.steps + 1,
+    }
+
+    if (x === 1 || x === 2) {
+      this.setState((state) => {
+        const originalIndex = state.grid.findIndex((activeSquare) =>
+          Boolean(activeSquare) ? activeSquare.toLowerCase() === "b" : null
+        );
+        const newGrid = state.grid.map(() => null);
+        newGrid[originalIndex + 1] = "B";
+
+        return {
+          ...state,
+          grid: newGrid,
+          message: "",
+          steps: state.steps + 1,
+        };
       });
     }
   };
 
-  handleReset = (e) => {
-    this.setState({
-      onSquare: 4,
-      x: 2,
-      y: 2,
-      steps: 0,
-      email: "",
+  handleReset = () => {
+    this.setState((state) => {
+      return {
+        ...state,
+        grid: [null, null, null, null, "B", null, null, null, null],
+        message: "",
+        steps: 0,
+        email: "",
+      };
+    });
+  };
+
+  getCoordinates = (grid) => {
+    let x = 2;
+    let y = 2;
+    const bIndex = grid.findIndex((activeSquare) =>
+      Boolean(activeSquare) ? activeSquare.toLowerCase() === "b" : null
+    );
+    if (bIndex >= 0 && bIndex <= 2) {
+      y = 1;
+    }
+    if (bIndex >= 3 && bIndex <= 5) {
+      y = 2;
+    }
+    if (bIndex >= 6 && bIndex <= 8) {
+      y = 3;
+    }
+
+    if (bIndex === 0 || bIndex === 3 || bIndex === 6) {
+      x = 1;
+    }
+    if (bIndex === 1 || bIndex === 4 || bIndex === 7) {
+      x = 2;
+    }
+    if (bIndex === 2 || bIndex === 5 || bIndex === 8) {
+      x = 3;
+    }
+    return [x, y];
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { grid, steps, email } = this.state;
+    const [x, y] = this.getCoordinates(grid);
+    axios
+      .post("http://localhost:9000/api/result", {
+        x,
+        y,
+        steps,
+        email,
+      })
+      .then((response) => {
+        this.setState((state) => {
+          return {
+            ...state,
+            email: "",
+            message: response.data.message,
+          };
+        });
+      })
+      .catch((error) => {
+        this.setState((state) => {
+          return {
+            ...state,
+            email: "",
+            message: error.response.data.message,
+          };
+        });
+      });
+  };
+
+  handleEmail = (event) => {
+    this.setState((state) => {
+      return {
+        ...state,
+        email: event.target.value,
+      };
     });
   };
 
   render() {
+    const { grid, steps, message, email } = this.state;
     const { className } = this.props;
+    const [x, y] = this.getCoordinates(grid);
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">
-            {`Coordinates (${this.state.x}, ${this.state.y})`}
-          </h3>
+          <h3 id="coordinates">{`Coordinates (${x},${y})`}</h3>
           <h3 id="steps">
-            {this.state.steps !== 1
-              ? `You moved ${this.state.steps} times`
-              : `You moved ${this.state.steps} time`}
+            {`You moved ${steps} time${steps !== 1 ? "s" : ""}`}
           </h3>
         </div>
         <div id="grid">
-          <div
-            className={`square ${this.state.onSquare === 0 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 0 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 1 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 1 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 2 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 2 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 3 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 3 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 4 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 4 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 5 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 5 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 6 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 6 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 7 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 7 ? "B" : ""}
-          </div>
-          <div
-            className={`square ${this.state.onSquare === 8 ? "active" : ""}`}
-          >
-            {this.state.onSquare === 8 ? "B" : ""}
-          </div>
+          {grid.map((activeSquare, i) => (
+            <div
+              key={i}
+              className={`square ${Boolean(activeSquare) ? "active" : ""}`}
+            >
+              {activeSquare}
+            </div>
+          ))}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={this.handleMoveLeft}>
@@ -313,9 +258,17 @@ export default class AppClass extends React.Component {
             reset
           </button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={this.handleEmail}
+          />
+          <button type="submit" id="submit">
+            Submit
+          </button>
         </form>
       </div>
     );
